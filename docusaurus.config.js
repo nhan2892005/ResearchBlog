28c2d8e0a9ba857757aacdd43c28e68442bccbbd
nhan2892005 <143471539+nhan2892005@users.noreply.github.com>
2024-10-17 19:21:13 +0700

    Update: social image from .png -> .jpg

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,7 +86,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/hpc.png',
+      image: 'img/hpc.jpg',
       navbar: {
         title: 'Research Blog',
         logo: {
